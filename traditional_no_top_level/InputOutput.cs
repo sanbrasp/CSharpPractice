4ae2bf19db1c84060b1bea0336dfaa369afa6b53
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-01-20 18:00:22 +0100

    Changed InputOutput.cs to comply with the framework name scheme, autodiscovery. ReverseString.cs test task for the framework.

---
+++ traditional_no_top_level/InputOutput.cs
@@ -0,0 +1,18 @@
+﻿using traditional_no_top_level.Menu;
+
+namespace traditional_no_top_level;
+
+public class InputOutput : ITask
+{
+    public string Title => "Input / Output";
+    public string Description => "Reads string input and prints it back to console.";
+    public string Subject => "Input/Output";
+    public int Number => 1;
+    
+    public void Run()
+    {
+        Console.WriteLine("Welcome to C# (Traditional)!");
+        Console.Write("What is your name? ");
+        string? name = Console.ReadLine();
+    }
+}
---
+++ traditional_no_top_level/ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using traditional_no_top_level.Menu;
+
+namespace traditional_no_top_level;
+
+public class ReverseString : ITask
+{
+    public string Title => "Reverse String";
+    public string Description => "Reads a string from console and prints the reverse.";
+    public string Subject => "Strings";
+    public int Number => 1;
+    
+    public void Run()
+    {
+        Console.WriteLine("Write some text: ");
+        var input = Console.ReadLine() ?? string.Empty;
+        
+        var reversed = new String(input.Reverse().ToArray());
+        Console.WriteLine("Reversed: " + reversed);
+    }
+}
