2421d28848896a6cfc8345d5b20ab983a6286c55
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-01-27 15:20:02 +0100

    Created a Person Class for encapsulation practice and controlled exposure of data using methods, propery practice using a setter with validation but no getter due to existing method.

---
+++ traditional_no_top_level/Models/Person.cs
@@ -0,0 +1,60 @@
+﻿namespace traditional_no_top_level.Models;
+
+public class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _birthYear;
+    private bool _isAlive;
+
+    public string LastName
+    {
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Last name can't be null or empty.");
+            _lastName = value;
+        }
+    }
+    
+    public Person(string firstName, string lastName, int birthYear)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("First name cannot be empty.", nameof(firstName));
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("Last name cannot be empty.", nameof(lastName));
+        if (birthYear < 1900 || birthYear > DateTime.Now.Year)
+            throw new ArgumentOutOfRangeException(nameof(birthYear), "Birth year is invalid.");
+        
+        _firstName = firstName;
+        _lastName = lastName;
+        _birthYear = birthYear;
+        _isAlive = true;
+    }
+
+    
+    public string ShowFullName()
+    {
+        return $"{_firstName} {_lastName}";
+    }
+
+    public int GetBirthYear()
+    {
+        return _birthYear;
+    }
+
+    public string IsPersonAlive()
+    {
+        if (_isAlive)
+            return "Person is currently alive.";
+        else
+        {
+            return "Person is dead.";
+        }
+    }
+    
+    public void PersonIsDead()
+    {
+        _isAlive = false;
+    }
+}
---
+++ traditional_no_top_level/Tasks/BuildAPerson.cs
@@ -0,0 +1,23 @@
+﻿using traditional_no_top_level.Menu;
+using traditional_no_top_level.Models;
+
+namespace traditional_no_top_level.Tasks;
+
+public class BuildAPerson : ITask
+{
+    public string Subject => "Classes";
+    public string Title => "Create a Person";
+    public string Description => "Create a person from the Person Class.";
+    public int Number => 2;
+
+    public void Run()
+    {
+        Person p1 = new Person("John", "Doe", 1987);
+
+        Console.WriteLine(p1.ShowFullName());
+        Console.WriteLine(p1.GetBirthYear());
+        Console.WriteLine(p1.IsPersonAlive());
+        p1.PersonIsDead();
+        Console.WriteLine(p1.IsPersonAlive());
+    }
+}
