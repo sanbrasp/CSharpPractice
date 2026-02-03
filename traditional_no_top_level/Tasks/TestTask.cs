6a0a9facf7a2f29a5f58b9f0907223415dfb5268
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-02-03 23:40:19 +0100

    Test task after making the task template work and show up in "new".

---
+++ traditional_no_top_level/Tasks/TestTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using traditional_no_top_level.Menu;
+
+namespace traditional_no_top_level.Tasks;
+
+public class TestTask : ITask
+{
+    public string Subject => "Test";
+    public string Title => "Test";
+    public string Description => "Testing task template";
+    public int Number => 1;
+
+    public void Run()
+    {
+        Console.WriteLine($"{Subject} - {Title} - {Description}");
+        Console.WriteLine("I created a task template that does all of the above\n" +
+                          "(sans actually writing the strings) for me. Very handy.");
+    }
+}
