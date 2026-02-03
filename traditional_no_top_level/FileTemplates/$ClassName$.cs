8844b6b50a82958f5bf81afba8a38a97c2532c1d
Sandra Nilsen Brattli-Aspvik <233507531+sanbrasp@users.noreply.github.com>
2026-02-03 23:20:07 +0100

    An attempt at a file template which I couldn't figure out so I commented it out for the time being.

---
+++ traditional_no_top_level/FileTemplates/$ClassName$.cs
@@ -0,0 +1,19 @@
+﻿// using System;
+// using traditional_no_top_level.Menu;
+//
+// namespace traditional_no_top_level.Templates;
+//
+// public class $ClassName$ : ITask
+// {
+// // ITask
+// public string Subject => "$Subject$";
+// public string Title => "$Title$";
+// public string Description => "$Description$";
+// public int Number => $Number$;
+//
+// public void Run()
+// {
+//     Console.WriteLine($"{Subject} - {Title} - {Description}");
+//     // TODO: Add task logic here
+// }
+// }
